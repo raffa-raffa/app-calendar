4bcc09cc313f9e1bc70d84035276c1d3bd8bedfa
Rafaela Cunha <rafaela.cunha@calindra.com.br>
2023-11-09 15:23:10 -0300

    Criando um componente que exibe calendario

---
+++ src/App.js
@@ -0,0 +1,18 @@
+import React, { Component } from 'react';
+import './App.css';
+import Calendar from './components/Calendar';
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <div className="App-header">
+          <h2>Calendar</h2>
+          <Calendar/>
+        </div>
+      </div>
+    );
+  }
+}
+
+export default App;
---
+++ src/components/Calendar.js
@@ -0,0 +1,28 @@
+import React , {useState } from "react";
+import DatePicker from 'react-datepicker';
+import 'react-datepicker/dist/react-datepicker.css';
+
+
+const Calendar = () =>{
+  const [startDate, setStartDate] = useState(new Date());
+  const [endDate, setEndDate] = useState(null);
+
+  const onChange = (dates) => {
+    const [start, end] = dates;
+    setStartDate(start);
+    setEndDate(end);
+  };
+
+  return (
+    <DatePicker
+      selected={startDate}
+      onChange={onChange}
+      startDate={startDate}
+      endDate={endDate}
+      selectsRange
+      inline
+    />
+  );
+};
+
+export default Calendar
